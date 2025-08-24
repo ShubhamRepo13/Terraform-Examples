768a45c586cd20a3a49adf2b91bec0f130d3e150
ShubhamRepo13 <shubhamschavhan247@gmail.com>
2025-08-24 07:36:06 +0530

    added internet connectivity

--- Chapter-1/main.tf
+++ Chapter-1/main.tf
@@ -3,9 +3,45 @@ provider "aws" {
 }
 
 resource "aws_instance" "Instance_creation" {
-  ami = "ami-0861f4e788f5069dd"
-  instance_type = "t2.micro"
+  ami                         = "ami-0861f4e788f5069dd"
+  instance_type               = "t2.micro"
+  vpc_security_group_ids      = ["${aws_security_group.Demo-sg.id}"]
+  associate_public_ip_address = true
+  user_data                   = <<-EOF
+#!/bin/bash
+sudo yum install -y httpd
+sudo  echo "Hello World" > sudo tee /var/www/html/index.html
+sudo systemctl start httpd
+sudo systemctl enable httpd
+EOF
+
   tags = {
     Name = "MyFirstInstance"
   }
+
+
 }
+
+resource "aws_security_group" "Demo-sg" {
+  name = "terraform-security-group"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
