4d1d7341c2d7db9fd2f508122d4c5d866e590255
ShubhamRepo13 <shubhamchavhan645@gmail.com>
2025-08-29 08:14:28 +0530

    added new files

---
+++ modules/services/webserver-cluster/main.tf
@@ -0,0 +1,187 @@
+
+terraform {
+  backend "s3" {
+    bucket         = "shubham-chavhan-s3-bucket-terraform-state"
+    key            = "stage/services/webserver-cluster/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "shubham-chavhan-terraform-locks"
+    encrypt        = true
+  }
+}
+
+# Get default VPC 
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Get subnet for ap-south-1a
+data "aws_subnet" "subnet_a" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+  filter {
+    name   = "availability-zone"
+    values = ["ap-south-1a"]
+  }
+}
+
+# Get subnet for ap-south-1b
+data "aws_subnet" "subnet_b" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+  filter {
+    name   = "availability-zone"
+    values = ["ap-south-1b"]
+  }
+}
+
+resource "aws_launch_template" "ASG_Launch_Template" {
+  name_prefix            = "ASG_Launch_Template"
+  image_id               = var.ami_id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = ["${aws_security_group.ASG_SG.id}"]
+  key_name               = var.key_name
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "ASG_Instance"
+    }
+  }
+  user_data = base64encode(templatefile("${path.module}/user-data.sh", {
+    server_port = var.server_port
+    db_address  = data.terraform_remote_state.s3.outputs.address
+    db_port     = data.terraform_remote_state.s3.outputs.port
+  }))
+
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group" "ASG_SG" {
+  name        = "ASG_Security_Group"
+  description = "Security group for Auto Scaling Group"
+  vpc_id      = data.aws_vpc.default.id
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.cidr_blocks
+  }
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = var.cidr_blocks
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = var.cidr_blocks
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+}
+
+#Auto Scaling Group
+resource "aws_autoscaling_group" "example_asg" {
+  name             = "example_asg_group"
+  desired_capacity = 2
+  min_size         = 1
+  max_size         = 2
+  vpc_zone_identifier = [data.aws_subnet.subnet_a.id, #ap-south-1a
+    data.aws_subnet.subnet_b.id                       #ap-south-1b 
+  ]
+
+  launch_template {
+    id      = aws_launch_template.ASG_Launch_Template.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "example_asg_instance"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_alb" "example_alb" {
+  name                       = "example-alb"
+  internal                   = false
+  load_balancer_type         = "application"
+  security_groups            = [aws_security_group.ASG_SG.id]
+  subnets                    = [data.aws_subnet.subnet_a.id, data.aws_subnet.subnet_b.id]
+  enable_deletion_protection = false
+  tags = {
+    Name = "example-alb"
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+
+}
+
+resource "aws_alb_target_group" "example_alb_tg" {
+  name     = "example-alb-tg"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
+  tags = {
+    Name = "example-alb-tg"
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+
+}
+
+resource "aws_alb_listener" "example_alb_listener" {
+  load_balancer_arn = aws_alb.example_alb.arn
+  port              = var.server_port
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_alb_target_group.example_alb_tg.arn
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+
+}
+
+resource "aws_autoscaling_attachment" "example_asg_attachment" {
+  autoscaling_group_name = aws_autoscaling_group.example_asg.name
+  lb_target_group_arn    = aws_alb_target_group.example_alb_tg.arn
+}
+
+data "terraform_remote_state" "s3" {
+  backend = "s3"
+  config = {
+    bucket = "shubham-chavhan-s3-bucket-terraform-state"
+    key    = "stage/data-store/mysql/terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
